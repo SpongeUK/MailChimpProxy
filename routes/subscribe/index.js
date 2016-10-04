30f232ab28f97f123c9b9305409fe15b7bb97e10
Chris Jones <chris.jones@spongeuk.com>
2016-10-04 10:36:53 +0100

    first commit

---
+++ routes/subscribe/index.js
@@ -0,0 +1,21 @@
+"use strict";
+/* global require, module */
+
+const express = require('express');
+const router = express.Router();
+const MailChimp = require('../lib/MailChimp');
+
+const settings = require('../settings');
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Subscription services are running.'})
+});
+
+router.post('/', function (req, res) {
+    const client = new MailChimp(settings.api.key, settings.api.host);
+    const subscriber = req.body.email;
+
+    client.addMemberToList(settings.list.id, subscriber);
+});
+
+module.exports = router;
---
+++ settings.js
@@ -0,0 +1,11 @@
+"use strict";
+/* global require, module, process */
+
+module.exports = {
+    "api": {
+        "key": process.env.API_KEY
+    },
+    "list": {
+        "id": process.env.LIST_ID
+    }
+};
