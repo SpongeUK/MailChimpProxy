f655539dcc4fed61cec244afb8dd17c84fd23d70
Chris Jones <chris.jones@spongeuk.com>
2016-10-04 10:50:08 +0100

    subscribe route

--- routes/subscribe/index.js
+++ routes/subscribe/index.js
@@ -3,9 +3,9 @@
 
 const express = require('express');
 const router = express.Router();
-const MailChimp = require('../lib/MailChimp');
+const MailChimp = require('../../lib/MailChimp');
 
-const settings = require('../settings');
+const settings = require('../../settings');
 
 router.get('/', function(req, res) {
     res.render('index', { title: 'Subscription services are running.'})
@@ -16,6 +16,7 @@ router.post('/', function (req, res) {
     const subscriber = req.body.email;
 
     client.addMemberToList(settings.list.id, subscriber);
+    res.sendStatus(200).send({ "status": "accepted", "email": subscriber });
 });
 
 module.exports = router;
